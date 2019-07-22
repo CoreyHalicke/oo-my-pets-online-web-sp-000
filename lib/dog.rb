84745684e97498e8f4281e3313f98fd10e55102e
Corey H <c.l.halicke@gmail.com>
2019-07-22 03:18:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,12 @@
 class Dog
   attr_reader :name
   attr_accessor :owner, :mood
+  @@all = []
 
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
+    @@all << self
   end
 end
